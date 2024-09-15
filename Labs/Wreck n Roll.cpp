6c8238b49d95940276e94429e3a87d570110f220
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2024-09-16 03:17:47 +0530

    Update Wreck n Roll.cpp

--- Labs/Wreck n Roll.cpp
+++ Labs/Wreck n Roll.cpp
@@ -1,3 +1,53 @@
+// In a 2D game, a stream of cars enters the screen, each with a different size and moving either right or left. Their direction is indicated by a sign - positive for right, negative for left.
+
+// When the cars moving in opposite directions collide head-on, the smaller car is destroyed. If two cars of the same size crash, they both get wrecked. However, if they’re moving in the same direction, they will not collide.
+
+// Determine which cars are still on the screen after all the collisions. Return the remaining cars in the order they appear.
+
+// Note: Initially, every car is at its index position i
+
+// Input Format
+
+// The first line of the input is an integer n, the total number of cars.
+
+// The second line of input consists of n integers, where each integer represents the size of a car. The sign of the integer (positive or negative) indicates the direction the car is moving in.
+
+// Constraints
+
+// 2 <= n <= 10^6
+
+// -10^5 <= carSize[i] <= 10^5
+
+// carSize[i] != 0
+
+// Output Format
+
+// Return the remaining cars on the screen in the order they appear (seperated by space) after all collisions.
+
+// Simply print 0, if there are no more remaining cars.
+
+// Sample Input 0
+
+// 3
+// 4 12 -6
+// Sample Output 0
+
+// 4 12
+// Sample Input 1
+
+// 4
+// 5 10 -10 -5
+// Sample Output 1
+
+// 0
+// Sample Input 2
+
+// 2
+// -10 41
+// Sample Output 2
+
+// -10 41
+
 
 #include <bits/stdc++.h>
 #include <queue>
