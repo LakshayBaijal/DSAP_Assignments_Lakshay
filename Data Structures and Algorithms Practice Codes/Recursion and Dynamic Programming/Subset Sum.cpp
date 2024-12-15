288bf0446805a64b5de2b53f0340f64e4543c095
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2024-12-16 02:26:31 +0530

    Update Subset Sum.cpp

--- Data Structures and Algorithms Practice Codes/Recursion and Dynamic Programming/Subset Sum.cpp
+++ Data Structures and Algorithms Practice Codes/Recursion and Dynamic Programming/Subset Sum.cpp
@@ -1,30 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
+vector<int> subsetSums(vector<int>& arr) 
+    {
+        // code here
+        vector<int>subset;
+        int n=arr.size();
+        int target=0;
+        int index=0;
+        subsetting(subset,index,n,target,arr);
+        
+        return subset;
+    }
+    
+    void subsetting(vector<int>&subset,int index,int size,int target,vector<int>&arr)
+    {
+        if(index==size)
+        {
+            subset.push_back(target);
+            return;
+        }
+        subsetting(subset,index+1,size,target,arr);
+        subsetting(subset,index+1,size,target+arr[index],arr);
 
-void comb(vector<int> &v, int index, int total) {
-  if (index < 0) {
-    cout << total << " ";
-    return;
-  }
-  int i = index;
-  comb(v, index - 1, total);
-  comb(v, index - 1, total + v[i]);
-}
-
-int main() {
-  vector<int> v;
-  int n;
-
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    int x;
-    cin >> x;
-    v.push_back(x);
-  }
-
-  sort(v.begin(), v.end());
-  int total = 0;
-
-  comb(v, n - 1, total);
-  return 0;
-}
+        return;
+    }
